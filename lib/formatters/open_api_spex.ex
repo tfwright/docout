24c3894bc659d4c986def3fc58f9649196bb1795
T Floyd Wright <tfwright@gmail.com>
2022-01-01 15:47:35 -0700

    Make OpenApiSpex formatter configurable

--- lib/formatters/open_api_spex.ex
+++ lib/formatters/open_api_spex.ex
@@ -4,21 +4,26 @@ defmodule Docout.Formatters.OpenApiSpex do
 
     Configure the base path for the api:
 
-    `config :docout, Docout.Formatters.OpenApiSpex, base_path: "/api/v1"`
+    ```
+    config :docout, Docout.Formatters.OpenApiSpex,
+                    base_path: "/api/v1",
+                    router: MyAppWeb.Router,
+                    spec: MyAppWeb.SpecModule
+    ```
   """
 
   def format(docs) do
     paths =
       docs
       |> Enum.flat_map(fn {mod, _func_docs} ->
-        BlocksCoreWeb.Router.__routes__()
+         Application.fetch_env!(:docout, Docout.Formatters.OpenApiSpex)[:router].__routes__()
         |> Enum.filter(&(mod == &1.plug))
       end)
       |> OpenApiSpex.Paths.from_routes()
       |> Enum.map(fn {path, def} -> {String.trim_leading(path, base_path()), def} end)
       |> Enum.into(%{})
 
-    BlocksCoreWeb.V1.Specs.spec()
+    Application.fetch_env!(:docout, Docout.Formatters.OpenApiSpex)[:spec].spec()
     |> Map.replace!(:paths, paths)
     |> Jason.encode!()
   end
