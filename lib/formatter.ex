cfc4483d82fc2164faad719ac702dc26d94f7b1d
T Floyd Wright <tfwright@gmail.com>
2022-01-02 14:56:29 -0700

    Improve formatter typespecs

--- lib/formatter.ex
+++ lib/formatter.ex
@@ -1,5 +1,9 @@
 defmodule Docout.Formatter do
   @moduledoc false
 
-  @callback format(List.t()) :: String.t()
+  @type func_docs :: [tuple()]
+  @type mod_name :: atom()
+  @type doc_list :: [{mod_name, func_docs}]
+
+  @callback format(doc_list) :: String.t()
 end
