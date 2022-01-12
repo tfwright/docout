8620bd2838115f71d407417a446a3814ca92dfdf
T Floyd Wright <tfwright@gmail.com>
2022-01-11 19:56:50 -0700

    Fix formatter spec

--- lib/formatter.ex
+++ lib/formatter.ex
@@ -3,7 +3,10 @@ defmodule Docout.Formatter do
 
   @type func_docs :: [tuple()]
   @type mod_name :: atom()
-  @type doc_list :: [{mod_name, func_docs}]
+  @type doc :: %{optional(binary()) => binary()} | :none | :hidden
+  @type metadata :: map()
+  @type docout_data :: {{kind :: atom(), function_name :: atom(), arity()}, function_doc :: doc, metadata}
+  @type doc_list :: [{module_doc :: doc, metadata, [docout_data]}]
 
   @callback format(doc_list) :: String.t()
 end
