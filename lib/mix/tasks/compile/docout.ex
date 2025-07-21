1413f8f2647a443755f417226078b010088ecc1d
T Floyd Wright <tfwright@gmail.com>
2025-07-21 10:19:05 -0800

    fix

--- lib/mix/tasks/compile/docout.ex
+++ lib/mix/tasks/compile/docout.ex
@@ -3,14 +3,14 @@ defmodule Mix.Tasks.Compile.Docout do
   use Mix.Task
 
   def run(_args) do
-    app_name = Application.compile_env!(:docout, :app_name)
+    app_name = Application.fetch_env!(:docout, :app_name)
 
     IO.inspect(app_name)
 
     :ok = Application.ensure_loaded(app_name)
 
-     :application.get_key(app_name)
-     |> IO.inspect(label: "loaded application")
+    :application.get_key(app_name)
+    |> IO.inspect(label: "loaded application")
 
     {:ok, modules} = :application.get_key(app_name, :modules)
 
